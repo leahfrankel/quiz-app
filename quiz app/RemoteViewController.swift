63e6a345e3410a0458879d1c4f05ff00950e62dc
David Horstman <davidzilla27@gmail.com>
2020-05-26 14:40:05 -0400

    Add second ViewController for results screen.

---
+++ quiz app/RemoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RemoteViewController.swift
+//  quiz app
+//
+//  Created by David Horstman on 5/26/20.
+//  Copyright © 2020 Leah Frankel. All rights reserved.
+//
+
+import UIKit
+
+class RemoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
