f45c26b1c8adaf8a607105c0e518509a56eddbdf
Leah <leahlu18@aol.com>
2020-05-26 20:39:59 -0400

    actions and outlets :)

--- quiz app/ViewController.swift
+++ quiz app/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var Answer0: UIButton!
+    @IBOutlet weak var Answer1: UIButton!
+    @IBOutlet weak var Answer2: UIButton!
+    @IBOutlet weak var Answer3: UIButton!
+    @IBAction func handleSubmitAnswer0(_ sender: Any) {
+    }
+    
+    @IBAction func handleSubmitAnswer1(_ sender: Any) {
+    }
+    
+    @IBAction func handleSubmitAnswer2(_ sender: Any) {
+    }
+    
+    @IBAction func handleSubmitAnswer3(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var progressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
