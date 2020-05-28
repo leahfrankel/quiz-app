988ed39af26e02de9b8e201b30bbf1ae2b64cf21
Leah <leahlu18@aol.com>
2020-05-28 10:45:29 -0400

    lots of new code

--- quiz app/ViewController.swift
+++ quiz app/ViewController.swift
@@ -28,11 +28,54 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var progressLabel: UILabel!
+    
+   func setQuestion() {
+      questionLabel.text = currentQuestion!.question
+      Answer0.setTitle(currentQuestion!.answers[0], for: .normal)
+      Answer1.setTitle(currentQuestion!.answers[1], for: .normal)
+      Answer2.setTitle(currentQuestion!.answers[2], for: .normal)
+      Answer3.setTitle(currentQuestion!.answers[3], for: .normal)
+      progressLabel.text = "Question \(currentQuestionPosition + 1) of \(questions.count)"
+    if ((currentQuestionPosition + 1) == questions.count) {
+             performSegue(withIdentifier: "sgShowResults", sender: nil)
+             }
+             else {
+        currentQuestionPosition += 1
+             currentQuestion = questions[currentQuestionPosition]
+             setQuestion() }
+       
+    }
+    
+    
+  
+    struct Question {
+        let question: String
+        let answers: [String]
+        let correctAnswer: Int
+    }
+    
+    var questions: [Question] = [
+    Question(
+        question: "What is my favorite color?",
+        answers: ["pink", "blue", "yellow", "green"],
+        correctAnswer: 2),
+]
+    var currentQuestion: Question?
+    var currentQuestionPosition: Int = 0
+    var noCorrect: Int = 0
+    
+
+    
+    //More instances of Question should be inside this array.
+    //You need one instance of Question for each question in your quiz.
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    
 }
 
