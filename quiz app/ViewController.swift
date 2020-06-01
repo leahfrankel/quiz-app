a73e345faa920f2e634c7ac0e636901b623ed382
Leah <leahlu18@aol.com>
2020-06-01 13:45:06 -0400

    testing!

--- quiz app/ViewController.swift
+++ quiz app/ViewController.swift
@@ -9,77 +9,103 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var Answer0: UIButton!
+    
     @IBOutlet weak var Answer1: UIButton!
+    
     @IBOutlet weak var Answer2: UIButton!
+ 
     @IBOutlet weak var Answer3: UIButton!
+    
     @IBAction func handleSubmitAnswer0(_ sender: Any) {
+               checkAnswer(choiceNo: 0)
     }
     
     @IBAction func handleSubmitAnswer1(_ sender: Any) {
+         checkAnswer(choiceNo: 1)
     }
     
     @IBAction func handleSubmitAnswer2(_ sender: Any) {
+         checkAnswer(choiceNo: 2)
     }
     
     @IBAction func handleSubmitAnswer3(_ sender: Any) {
+         checkAnswer(choiceNo: 3)
     }
     
     @IBOutlet weak var progressLabel: UILabel!
     
-   func setQuestion() {
-      questionLabel.text = currentQuestion!.question
-      Answer0.setTitle(currentQuestion!.answers[0], for: .normal)
-      Answer1.setTitle(currentQuestion!.answers[1], for: .normal)
-      Answer2.setTitle(currentQuestion!.answers[2], for: .normal)
-      Answer3.setTitle(currentQuestion!.answers[3], for: .normal)
-      progressLabel.text = "Question \(currentQuestionPosition + 1) of \(questions.count)"
-    
+    func setQuestion() {
+        questionLabel.text = currentQuestion!.question
+        Answer0.setTitle(currentQuestion!.answers[0], for: .normal)
+        Answer1.setTitle(currentQuestion!.answers[1], for: .normal)
+        Answer2.setTitle(currentQuestion!.answers[2], for: .normal)
+        Answer3.setTitle(currentQuestion!.answers[3], for: .normal)
+        progressLabel.text = "Question \(currentQuestionPosition + 1) of \(questions.count)"
+        
+    }
     //loadnextquestion:
-    if ((currentQuestionPosition + 1) == questions.count) {
-             performSegue(withIdentifier: "sgShowResults", sender: nil)
-             }
-             else {
-        currentQuestionPosition += 1
-             currentQuestion = questions[currentQuestionPosition]
-             setQuestion() }
-    
-    //checkanswer:
-    
+    func loadNextQuestion(){
+        if ((currentQuestionPosition + 1) == questions.count) {
+            performSegue(withIdentifier: "sgShowResults", sender: nil)
+        }
+        else {
+            currentQuestionPosition += 1
+            currentQuestion = questions[currentQuestionPosition]
+            setQuestion() }
+    }
+
+func checkAnswer(choiceNo: Int){
+    if (choiceNo == currentQuestion!.correctAnswer) {
+        noCorrect += 1
+        loadNextQuestion()
+    }
+    else {
+        loadNextQuestion()
     }
     
+}
+
+
     
-  
-    struct Question {
-        let question: String
-        let answers: [String]
-        let correctAnswer: Int
-    }
-    
-    var questions: [Question] = [
+    //addnewhere
+
+//checkanswer:
+struct Question {
+    let question: String
+    let answers: [String]
+    let correctAnswer: Int
+}
+
+var questions: [Question] = [
     Question(
         question: "What is my favorite color?",
         answers: ["pink", "blue", "yellow", "green"],
         correctAnswer: 2),
 ]
-    var currentQuestion: Question?
-    var currentQuestionPosition: Int = 0
-    var noCorrect: Int = 0
-    
+var currentQuestion: Question?
+var currentQuestionPosition: Int = 0
+var noCorrect: Int = 0
 
-    
-    //More instances of Question should be inside this array.
-    //You need one instance of Question for each question in your quiz.
 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-  
-    
+//More instances of Question should be inside this array.
+//You need one instance of Question for each question in your quiz.
+
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view
+    // Setup the question and reset currentQuestion
+    currentQuestion = questions[0]
+    setQuestion()
+
 }
+
+
+
+}
+
 
